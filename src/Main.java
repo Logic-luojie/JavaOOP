306c823b06e795c814e6ececc02eb3b51e16584a
Lambert <2638498951@qq.com>
2022-06-23 22:02:26 +0800

    result commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Roger_Linjj
+ * @create 2022-06-23 21:56
+ */
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("小明",18,"男",1);
+        System.out.println(student);
+    }
+}
